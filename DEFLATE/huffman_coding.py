4e3cf1f17eb723ae83872e7bdcd89fa5383490ea
Mask <shanesoh0077i@gmail.com>
2026-03-01 23:08:09 +0800

    Update and rename huffman_coding_encoding.py to huffman_coding.py

--- DEFLATE/huffman_coding.py
+++ DEFLATE/huffman_coding.py
@@ -1,4 +1,4 @@
-# Huffman coding encoding
+# Huffman encoding
 # Intuition: Suppose you have a string, and some characters appear more frequently than others, but all of them takes up the same bit size (E.g. 'looool' -> 110110011011111101111110111111011111101100)
 # Solution: We can prioritise them based on frequency, with the smallest bit size being the most common and the largest bit size being the rarest (E.g. 'looool' -> 011110)
 # Prerequisite: Binary trees, Priority Queue/Min Heap* [Not necessary to implement]
@@ -77,3 +77,24 @@ print("UTF-8 conversion table: ", dictionary)
 print("Huffman code: ", encoded)
 print("Huffman conversion table: ", codes)
 print("As you can see, it stores the same text with fewer bits.")
+
+# Huffman decoding
+# The only reason why Huffman decoding works is because it's a form of prefix code
+# No code is a PREFIX of another code
+# Try decoding the Huffman code if 's' : '1'. It's not possible :]
+
+# For those that are sharp, different files using huffman encoding creates a different tree (since frequency is different)
+# So how is it possible that my file explorer can decode/unzip a random zipped file?
+# In fact, the image contains metadata on the frequency count (calculate_frequency()), allowing it to recreate the tree and reverse it
+def huffman_decoding(encoded, codes):
+    decoded = []
+    current_code = ''
+    code_to_char = {v: k for k, v in codes.items()} # Invert the codes
+    for bit in encoded:
+        current_code += bit
+        if current_code in code_to_char: # Matched bits to known combination in dictionary
+            decoded.append(code_to_char[current_code])
+            current_code = '' # Reset
+    return ''.join(decoded)
+decoded = huffman_decoding(encoded, codes) # As you can see, we need two parameters.
+print("Decoded text: ", decoded) # Although you have the encoded message, you need to know the conversion table/codes or we can't decode it
